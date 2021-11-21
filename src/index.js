f2035f7a35aa2a16dde79c927f6a9a1b46abf7c7
amar1057 <amar1057@stu.southtexascollege.edu>
2021-11-21 08:26:41 -0600

    Error Handler added

--- src/index.js
+++ src/index.js
@@ -5,15 +5,20 @@ import cors from 'cors'
 
 import config from './utils/config'
 import logger from './utils/logger'
+import errors from './utils/errors'
 
 const app = express()
 
 app.use(logger.middleware)
 app.use(helmet())
 app.use(cors({ origin: config.origin }))
+
 app.get('/', (req, res) => {
   logger.log.success('Calling Root')
   res.send({ msg: 'hello there!' })
 })
+
+app.use(errors.notFound)
+app.use(errors.errorHandler)
 
 app.listen(config.port)
